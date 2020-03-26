079818d5fea507fcb8abab86d8069057c537175f
Snow1226 <snow.lapisis@gmail.com>
2020-03-27 03:16:20 +0900

    Support for game version 1.8.0

--- CameraPlus/TransparentWallsPatch.cs
+++ CameraPlus/TransparentWallsPatch.cs
@@ -10,7 +10,7 @@ namespace CameraPlus
         public static int WallLayerMask = 25;
         private static void Postfix(ref ObstacleController __instance)
         {
-            Camera.main.cullingMask |= (1 << TransparentWallsPatch.WallLayerMask);//Enables HMD bits because layer 26 is masked by default
+            Camera.main.cullingMask |= (1 << TransparentWallsPatch.WallLayerMask);//Enables HMD bits because layer 25 is masked by default
             Renderer mesh = __instance.gameObject?.GetComponentInChildren<Renderer>(false);
             if (mesh?.gameObject)
             {
